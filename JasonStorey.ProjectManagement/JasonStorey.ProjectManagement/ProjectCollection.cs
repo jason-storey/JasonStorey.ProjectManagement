ec62a253f028f1c3edf73863da1ea34e7f5287fd
Jason Storey <jasonstoreyireland@gmail.com>
2022-11-12 11:46:02 +0000

    Adds Initial Project and Unit Tests

---
+++ JasonStorey.ProjectManagement/JasonStorey.ProjectManagement/ProjectCollection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace JasonStorey.ProjectManagement
+{
+    public class ProjectCollection : IEnumerable<Project>
+    {
+        #region plumbing
+
+        public int Count => _projects.Count;
+        public ProjectCollection() => _projects = new List<Project>();
+        public IEnumerator<Project> GetEnumerator() => _projects.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        readonly List<Project> _projects;
+        
+
+        #endregion
+    }
+}
